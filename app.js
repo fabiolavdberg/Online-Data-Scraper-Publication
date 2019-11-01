6bac5490d10a6c41bef7a48fcab94c9878a9c000
Fabiola <fabiola-vandenberg@hotmail.com>
2019-11-01 13:37:03 +0100

    update

--- app.js
+++ app.js
@@ -1,19 +1,35 @@
 const Crawler = require("crawler");
 const fs = require('fs');
+// var port = 8887;
 
-const BESTAND = "sample.txt"
+
+const BESTAND = "sample.js"
 const pages = [
     'https://transcripts.foreverdreaming.org/viewtopic.php?f=845&t=31373',
     'https://transcripts.foreverdreaming.org/viewtopic.php?f=845&t=31374',
+    'http://transcripts.foreverdreaming.org/viewtopic.php?f=845&t=31375',
+    'http://transcripts.foreverdreaming.org/viewtopic.php?f=845&t=31376',
+    'http://transcripts.foreverdreaming.org/viewtopic.php?f=845&t=31377',
+    'http://transcripts.foreverdreaming.org/viewtopic.php?f=845&t=31378',
+    'http://transcripts.foreverdreaming.org/viewtopic.php?f=845&t=31379',
+    'http://transcripts.foreverdreaming.org/viewtopic.php?f=845&t=31380',
+    'http://transcripts.foreverdreaming.org/viewtopic.php?f=845&t=31385',
+
 ]
 
 const hits = {
-    "MONICA": [],
-    "JOEY": [],
-    "CHANDLER": [],
-    "ROSS": [],
-    "RACHEL": [],
-    "PHOEBE": [],
+    MONICA: [],
+    JOEY: [],
+    CHANDLER: [],
+    ROSS: [],
+    RACHEL: [],
+    PHOEBE: [],
+    monica: [],
+    joey:[],
+    chandler:[],
+    ross: [],
+    rachel: [],
+    phoebe: [],
 };
 
 const c = new Crawler({
@@ -33,11 +49,10 @@ const c = new Crawler({
                     hits[key].push(line);
                 })
             });
-
-
+        
 
             fs.writeFile(BESTAND, '' , function() {})
-            fs.writeFile(BESTAND, JSON.stringify(hits, null, 4), function() {});
+            fs.writeFile(BESTAND, "var friends_data = " + JSON.stringify(hits, null, 4), function() {});
         }
         done();
     }
